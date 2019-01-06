fcfbe706a0b79ac410e771356d7bfa481c385f8e
hendyramadhany <hendyramadhnay@gmail.com>
2019-01-06 23:48:27 +0700

    update koneksi database

--- src/Koneksi.java
+++ src/Koneksi.java
@@ -6,8 +6,30 @@
 
 /**
  *
- * @author User
+ * @author Teuku Ngentod hehe
  */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Koneksi {
-    
+    public Connection conn;
+    public void connect() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver Database ditemukan");
+        } catch (Exception e) {
+            System.out.println("Error Driver Tidak ditemukan " + e);
+        }
+        String urlDatabase = "jdbc:mysql://localhost/nama_database";
+        String user = "root";
+        String password = "";
+        
+        try {
+            conn = DriverManager.getConnection(urlDatabase, user, password);
+            System.out.println("Berhasil Terhubung ke Database");
+        } catch (Exception e) {
+            System.out.println("Gagal Terhubung ke Database " + e);
+        }
+    }
 }
